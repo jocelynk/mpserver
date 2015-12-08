ec294466a9fec5e7cc6edcd9e49df65c6ed0b651
jocelynk <jocey.kong@gmail.com>
2015-12-08 12:42:58 -0500

    switching back to db

--- models/db.js
+++ models/db.js
@@ -1,12 +1,10 @@
 var mongoose = require('mongoose');
 
 //database
-mongoose.connect('mongodb://localhost/mp', function(err) {
+mongoose.connect('mongodb://heroku_p43wgxfq:6uobk9jdbtu7d067qqh09caubl@ds035014.mongolab.com:35014/heroku_p43wgxfq', function(err) {
     if(err) {
         console.log('connection error', err);
     } else {
         console.log('connection successful');
     }
 });
-
-//heroku_p43wgxfq:6uobk9jdbtu7d067qqh09caubl@ds035014.mongolab.com:35014/heroku_p43wgxfq
